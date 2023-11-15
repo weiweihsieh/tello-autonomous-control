44c50ab8aa7d8845fc696369df4c4ea5f81c2869
weiwei <hsieh9031786@gmail.com>
2023-11-15 17:05:56 +0800

    Stop tracking files

--- h264_image_transport/src/manifest.cpp
+++
@@ -1,5 +0,0 @@
-#include <pluginlib/class_list_macros.h>
-#include <h264_image_transport/h264_subscriber.h>
-
-PLUGINLIB_EXPORT_CLASS(h264_image_transport::H264Subscriber, image_transport::SubscriberPlugin)
-
